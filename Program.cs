ef882669e1c865a6356859c938f00e3c476daa8f
Yalcin <bilgekagan902@gmail.com>
2024-02-28 18:17:28 +0300

    hata yönetimi

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace hata_yonetimi{
+    class Program{
+        public static void Main(string[] args){
+            try{
+                int a = int.Parse("-2000000000");
+            }
+            catch(ArgumentNullException ex){
+                Console.WriteLine("Boş deger girdiniz");
+                Console.WriteLine(ex);
+            }
+            catch(FormatException ex){
+                Console.WriteLine("Veri tipi uygun değil");
+                Console.WriteLine(ex);
+            }
+            catch(OverflowException ex){
+                Console.WriteLine("Cok kucuk veya buyuk deger girdiniz");
+                Console.WriteLine(ex);
+            }
+            finally{
+                Console.WriteLine("islem basarili");
+            }
+        }
+    }
+}
